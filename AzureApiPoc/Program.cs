023bd3a50b9696e00668a7beb18d868a01d64d90
Trimmy Jimmy <github@jimh.co>
2025-03-12 12:26:59 +0000

    Update Program.cs

--- AzureApiPoc/Program.cs
+++ AzureApiPoc/Program.cs
@@ -11,8 +11,7 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        services.AddSingleton<IQueueClient>(client => new QueueClient(connectionString: "Endpoint=sb://zz-sb-bob-cwpsa-api.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=secret_key=",
-                                                                      entityPath: "zz-sbq-bob-cwpsa-api"));
+
         services.AddSingleton<IMessagePublisher, MessagePublisher>();
     })
     .Build();
